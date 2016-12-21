4d657dbf2c927032399792432b10394838ed7a77
Samuel Christie <emailium@gmail.com>
2016-12-21 17:27:10 -0500

    Create new logic module for named statements

---
+++ protocheck/logic.py
@@ -0,0 +1,61 @@
+"""
+Logic operations for combining objects that represent named statements, to improve readability.
+
+Named statements are represented as name/statement pairs in a dict.
+Because conjunction is more common, it is represented my merging dicts.
+Disjunction is represented by a list instead.
+
+E.x.:
+>>> a = Name(a, "A")
+{"A": a}
+>>> b = Name(b, "B")
+{"B": b}
+>>> And(a,b)
+{"A": a, "B": b}
+>>> Or(a,b)
+[{"A":a}, {"B":b}]
+"""
+
+import boolexpr as bx
+from boolexpr import *
+from itertools import combinations, permutations, chain
+
+def merge(*dicts):
+    """
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in latter dicts.
+    """
+    result = {}
+    for dictionary in dicts:
+        result.update(dictionary)
+    return result
+
+def Name(statement, name):
+    return {name: statement}
+
+def And(*statements):
+    o = {}
+    for s in statements:
+        o = merge(o, s)
+    return o
+
+def Or(*statements):
+    return statements
+
+def compile(statements):
+    if type(statements) is list or type(statements) is tuple:
+        # disjunction
+        return or_s(*[compile(s) for s in statements])
+    elif type(statements) is dict:
+        # conjunction
+        return and_s(*[compile(s) for s in statements.values()])
+    else:
+        # atomic statement
+        return statements
+
+def named(fn):
+    """Wrapper for functions that return logic statements, to track where they came from"""
+    def wrapped(*args, **kwds):
+        name = "-".join([a.name for a in args] + [fn.__name__])
+        return Name(fn(*args, **kwds), name)
+    return wrapped
