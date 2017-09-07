74f02e1901b09624de9a35e715c063fe9a916046
Samuel Christie <emailium@gmail.com>
2017-09-07 11:01:41 -0400

    Clean up styling to match flymake expectations

--- protocheck/test/precedence_test.py
+++ protocheck/test/precedence_test.py
@@ -1,5 +1,10 @@
 import pytest
-from protocheck.precedence import *
+from boolexpr import and_, not_
+from protocheck.precedence import (
+    transitivity, relationships, var,
+    occurrence, simultaneous, sequential, extract_events
+)
+
 
 def test_occurrence():
     occ = occurrence(*extract_events(simultaneous('a', 'b')))
@@ -8,8 +13,9 @@ def test_occurrence():
     print('formula: ', formula)
     s = formula.sat()[1]
     if s:
-        print('solution: ', [k for k,v in s.items() if v])
+        print('solution: ', [k for k, v in s.items() if v])
     assert not s
+
 
 def test_relationships():
     statements = [simultaneous('a', 'b'), sequential('a', 'b')]
