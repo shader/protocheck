1efd843a9bbaf3ccf7869572448e9dc8a06f83d4
Samuel Christie <emailium@gmail.com>
2017-09-18 22:41:02 -0400

    Fix use of consistent in precedence_test.py

--- protocheck/test/precedence_test.py
+++ protocheck/test/precedence_test.py
@@ -122,26 +122,20 @@ def chain(n):
 
 
 def test_consistent():
-    def check(cs):
-        return consistent(cs).sat()[1]
-
     # check basic consistency
-    assert consistent(var('a'), var('b'), sequential('a', 'b')).sat()[0]
+    assert consistent(var('a'), var('b'), sequential('a', 'b'))
 
     # see how long a causal loop can be before transitivity stops working
     for i in range(1, 5):
         clauses = [sequential(to_char(i), 'a')] + chain(i)
-        assert not check(and_(*clauses))
+        assert not consistent(and_(*clauses))
 
 
 def test_exhaustive_consistent():
-    def check(cs):
-        return consistent(cs, exhaustive=True).sat()[1]
-
     # check basic consistency
-    assert consistent(var('a'), var('b'), sequential('a', 'b')).sat()[0]
+    assert consistent(var('a'), var('b'), sequential('a', 'b'))
 
     # see how long a causal loop can be before transitivity stops working
     for i in range(1, 5):
         clauses = [sequential(to_char(i), 'a')] + chain(i)
-        assert not check(and_(*clauses))
+        assert not consistent(and_(*clauses), exhaustive=True)
